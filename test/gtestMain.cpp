0f062790f61898e6db7c566bad0d541b86abe5c7
paul leclair <34226667+jabronicus@users.noreply.github.com>
2023-06-11 23:15:31 -0600

    Update gtestMain.cpp

--- test/gtestMain.cpp
+++ test/gtestMain.cpp
@@ -1,5 +1,5 @@
 //
-// Created by paull on 2022-07-04.
+// TODO - evaluate whether we need this or whether we can use the default gtest main
 //
 
 #ifdef _WIN32
@@ -11,10 +11,6 @@
 #include <gtest/gtest.h>
 
 INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow ) {
-
-  std::cout << "Pee Engine, Test!" << std::endl;
-
-//  ::testing::InitGoogleTest((int*)&nCmdShow, (char **)lpCmdLine);
 
   ::testing::InitGoogleTest();
 
@@ -29,14 +25,9 @@ INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine
 // LINUX/MAC(?) ENTRY POINT
 int main( int argc, char *argv[] ) {
 
-  std::cout << "Pee Engine, Activate!" << std::endl;
-
-  std::cout << "Pee Engine, Test!" << std::endl;
-
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
-
-
+  
   return 0;
 }
 #endif

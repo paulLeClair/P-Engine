0bffa4d523eb8fb88c198a0f2666340fc64f4e9e
paul leclair <34226667+jabronicus@users.noreply.github.com>
2025-08-04 01:19:04 -0600

    Update Renderer.hpp

--- src/GraphicsEngine/Backend/Renderer/Renderer.hpp
+++ src/GraphicsEngine/Backend/Renderer/Renderer.hpp
@@ -8,20 +8,7 @@
 #include <vector>
 
 namespace pEngine::girEngine::backend {
-    /**
-     * We're finally here!
-     *
-     * I think it might make sense to rename this whole "Renderer" thing (which I don't like as much now)
-     * to something like a more general "Renderer" or something;
-     *
-     * If you think about it, the main purpose of this whole abstraction is to just take the stuff that was built up
-     * at bake time and start drawing frames according to however it has been configured.
-     *
-     * It's where the actual rendering happens, and I think that's the main reason it's separated from the app context
-     * in the first place.
-     *
-     * So yeah fuck it I'm goin for it
-     */
+    // stub interface; probably will be removed
     class Renderer {
     public:
         virtual ~Renderer() = default;
